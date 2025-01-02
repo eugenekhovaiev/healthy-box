22f6d4191553042d0847a66db18a16359ecf1ee4
Yevhenii Khovaiev <ekhovaev@gmail.com>
2025-01-03 00:38:48 +0200

    feat: add delivery and help sections on rations page

--- src/pages/Rations/Rations.tsx
+++ src/pages/Rations/Rations.tsx
@@ -1,3 +1,5 @@
+import { Delivery } from '../../components/sections/Delivery';
+import { Help } from '../../components/sections/Help';
 import { UsualBanner } from '../../components/sections/UsualBanner';
 import { RationsInfo } from './sections/RationsInfo';
 
@@ -6,6 +8,8 @@ export const Rations = () => {
     <main>
       <UsualBanner pageName="Раціони" />
       <RationsInfo />
+      <Delivery />
+      <Help />
     </main>
   );
 };
